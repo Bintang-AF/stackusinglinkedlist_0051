a226ff8de7ee590fc061a0f35b8d4d875d99c9c5
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-06-05 10:39:25 +0700

    cek jika  stack kosong

--- stack.cpp
+++ stack.cpp
@@ -25,4 +25,17 @@ class stack
     }
 
     int push(int value)
-    
+    {
+        Node *newNode = new Node ();
+        newNode -> data = value;
+        newNode -> next = top;
+        top = newNode ;
+        cout << "push value: " << value << endl;
+        return value ;
+    }
+
+    bool isEmpty()
+    {
+        return top == NULL;
+    }
+};
